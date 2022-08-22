3dd29b3073152bf8448b06f27575981dec7a7121
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-08-22 17:16:47 -0400

    probando sin validates

--- app/models/edificio.rb
+++ app/models/edificio.rb
@@ -2,9 +2,11 @@ class Edificio < ApplicationRecord
     has_many :departamentos, dependent: :delete_all
     #aqui sabe cual es el nombre de la clave foranea
     #al modelo en plural en que clase y tabla
-    validates :nombre, presence: true
-    validates :direccion, presence: true
-    validates :ciudad, presence: true
+    #validates :nombre, presence: true
+    #validates :direccion, presence: true
+    #validates :ciudad, presence: true
+
+    paginates_per 20
     # validaciones para que no se guarde el objeto vacío
     #def Departamento
     #    Departamento.where(edificio_id: self.id)
