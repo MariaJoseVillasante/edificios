f7e469afd318ccb97052b648f4465be789cd6244
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-08-24 16:36:44 -0400

    eliminando restricciones

--- db/migrate/20220801185944_add_column_to_departamento.rb
+++ db/migrate/20220801185944_add_column_to_departamento.rb
@@ -1,5 +1,5 @@
 class AddColumnToDepartamento < ActiveRecord::Migration[7.0]
   def change
-    add_reference :departamentos, :cliente, null: false, foreign_key: true
+    add_reference :departamentos, :cliente, null: false, foreign_key: false
   end
 end
